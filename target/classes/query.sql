4200a58b35840a8d2382e507cf303b5f1a2ab749
SergeyErmakovich <sergeyermakovich@coherentsolutions.com>
2019-12-23 14:03:42 +0300

    some changes

--- target/classes/query.sql
+++ target/classes/query.sql
@@ -4,6 +4,6 @@ create table message
   text VARCHAR(128) not null
 ) ;
 
-insert into message values(1, 'The Tartar Steppe');
-insert into message values(2, 'Poem Strip');
-insert into message values(3, 'Restless Nights: Selected Stories of Dino Buzzati');
+insert into message values(1, 'moite ruki');
+insert into message values(2, 'plez');
+insert into message values(3, 'mfrkf');
